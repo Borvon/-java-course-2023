cc37ab2e20cd3389dcc9ea7e4a1420519775e3ba
Borvon <borvon@yandex.ru>
2023-12-18 02:18:31 +0700

    hw11

---
+++ src/test/java/edu/hw11/task2/ArithmeticUtilsChanger.java
@@ -0,0 +1,46 @@
+package edu.hw11.task2;
+
+import edu.hw11.task1.HelloWorld;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import static net.bytebuddy.dynamic.loading.ByteArrayClassLoader.load;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArithmeticUtilsChanger {
+
+    @Test
+    void test() {
+
+        try (DynamicType.Unloaded<?> dynamicType = new ByteBuddy()
+            .subclass(ArithmeticUtils.class)
+            .method(ElementMatchers.named("sum"))
+            .intercept(Advice.to(ArithmeticAdvice.class))
+            .make()) {
+
+            Class<?> loadedClass = dynamicType
+                .load(ArithmeticUtils.class.getClassLoader())
+                .getLoaded();
+
+            Constructor<?> constructor = loadedClass.getDeclaredConstructor();
+            Object instance = constructor.newInstance();
+
+
+            Method sumMethod = loadedClass.getDeclaredMethod("sum", int.class, int.class);
+
+            assertThat((int) sumMethod.invoke(instance, 4, 5)).isEqualTo(20);
+
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
